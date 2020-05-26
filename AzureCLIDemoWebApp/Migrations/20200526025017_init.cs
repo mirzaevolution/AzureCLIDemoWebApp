62d5efc81807f87aa7ccacf0b4c10e19b3e15376
Mirza Ghulam Rasyid <mgr@voxteneo.asia>
2020-05-26 09:56:05 +0700

    Adding migrations

---
+++ AzureCLIDemoWebApp/Migrations/20200526025017_init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AzureCLIDemoWebApp.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Position = table.Column<string>(nullable: true),
+                    Office = table.Column<string>(nullable: true),
+                    Extension = table.Column<string>(nullable: true),
+                    Salary = table.Column<decimal>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
